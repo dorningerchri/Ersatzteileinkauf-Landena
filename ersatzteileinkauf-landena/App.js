c95c4233457b6e6a4728f2c49e69708e8741d872
dorningerchri <dorninger.chri@gmail.com>
2025-08-01 10:54:07 +0200

    Add files via upload

---
+++ ersatzteileinkauf-landena/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [orders, setOrders] = useState([]);
+
+  return (
+    <div>
+      <h1>Ersatzteileinkauf Landena</h1>
+      {/* Formular und Tabelle folgen */}
+    </div>
+  );
+}
